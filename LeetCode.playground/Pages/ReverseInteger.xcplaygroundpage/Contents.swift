531452cdee2c94dfc5ecdf6907d5a399f33170e7
Go Kameda <go.kameda@icloud.com>
2019-05-12 19:02:00 +0900

    Safeguard for integer overflow

--- LeetCode.playground/Pages/ReverseInteger.xcplaygroundpage/Contents.swift
+++ LeetCode.playground/Pages/ReverseInteger.xcplaygroundpage/Contents.swift
@@ -1,12 +1,21 @@
 import Foundation
 
 final class Solution {
+    //let min: Int = Int(truncating: NSDecimalNumber(decimal: pow(-2, 31)))
+    //let max: Int = Int(truncating: NSDecimalNumber(decimal: pow(2, 31) - 1))
     func reverse(_ x: Int) -> Int {
+        guard x < Int32.max else { return 0 }
+        guard x > Int32.min else { return 0 }
         var n = 1
         var p10 = 1, p10X = 1
         var a = [Int]()
         while true {
-            p10 *= 10
+            let t = p10.multipliedReportingOverflow(by: 10)
+            guard t.overflow == false else { return 0 }
+            let p10New = t.partialValue
+            guard p10New < Int32.max else { return 0 }
+            guard p10New > Int32.min else { return 0 }
+            p10 = p10New
             //print("p10: \(p10)")
             let m = (x % p10)
             //print("m: \(m)")
@@ -19,7 +28,17 @@ final class Solution {
         }
         var ret = 0
         for n in a {
-            ret += n * p10X
+            let t1 = p10X.multipliedReportingOverflow(by: n)
+            guard t1.overflow == false else { return 0 }
+            let p10n = t1.partialValue
+            guard p10n < Int32.max else { return 0 }
+            guard p10n > Int32.min else { return 0 }
+            let t2 = ret.addingReportingOverflow(p10n)
+            guard t2.overflow == false else { return 0 }
+            let newRet = t2.partialValue
+            guard newRet < Int32.max else { return 0 }
+            guard newRet > Int32.min else { return 0 }
+            ret = newRet
             p10X /= 10
         }
         return ret
@@ -40,7 +59,7 @@ final class Solution {
 
 let solution = Solution()
 
-let x = 123456789
+let x = 120
 let ans = solution.reverse(x)
 print("ans: \(ans)")
 
