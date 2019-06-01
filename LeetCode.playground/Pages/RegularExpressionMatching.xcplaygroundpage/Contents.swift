15f08976af643af3e03d0c089f05db96535b9c13
Go Kameda <go.kameda@icloud.com>
2019-06-01 17:50:28 +0900

    Fix implementation error (After all, define two different counter s, p and re-doing from basic logic)

--- LeetCode.playground/Pages/RegularExpressionMatching.xcplaygroundpage/Contents.swift
+++ LeetCode.playground/Pages/RegularExpressionMatching.xcplaygroundpage/Contents.swift
@@ -3,67 +3,57 @@ import Foundation
 final class Solution {
     func isMatch(_ s: String, _ p: String) -> Bool {
         let sChars = Array(s)
+        let sCount = sChars.count
         let pChars = Array(p)
-
-        var i = 0
+        let pCount = pChars.count
+        var s = 0, p = 0
         var pCarry: Character?
-        for sChar in sChars {
-            //print("sChar: \(sChar)")
-            guard i < pChars.count else {
-                if let pCarryChar = pCarry {
-                    //print("pCarryChar: \(pCarryChar)")
-                    if pCarryChar == "." {
-                        continue
-                    } else if sChar == pCarryChar {
-                        continue
-                    } else {
-                        return false
-                    }
-                } else {
-                    return false
-                }
-            }
+        while s < sCount {
+            print("s: \(s)")
+            print("p: \(p)")
 
-            let pChar = pChars[i]
-            //print("pChar: \(pChar)")
-            i += 1
+            let sChar = sChars[s]
+            print("sChar: \(sChar)")
 
-            if i == sChars.count {
-                if i < pChars.count && pChars[i] != "*" { return false }
-            }
+            let pChar = pChars[p]
+            print("pChar: \(pChar)")
+            p += 1
 
             if pChar == "*" {
-                guard let pCarryChar = pCarry else {
-                    //print("Invalid Operation: pCarry is nil.");
-                    return false
-                }
-                //print("pCarryChar: \(pCarryChar)")
-                if pCarryChar == "." {
+                let uPCarry = pCarry!
+                s += 1
+                print("uPCarry: \(uPCarry)")
+                if uPCarry == "." {
                     continue
-                } else if pCarryChar == sChar {
+                } else if uPCarry == sChar {
                     continue
                 } else {
                     pCarry = nil
-                    guard i < pChars.count else { return false }
-                    //print("pChars[i]: \(pChars[i])")
-                    guard pChars[i] == sChar else { return false }
+                    guard p < pCount else { print("=== A ==="); return false }
+                    print("pChars[p]: \(pChars[p])")
+                    guard pChars[p] == sChar else { print("=== B ==="); return false }
                 }
             } else {
-                if i < pChars.count && pChars[i] == "*" {
+                if p < pCount && pChars[p] == "*" {
                     pCarry = pChar
+                    // s += 1 // NOT NEEDED
+                    print("pCarry: \(pCarry!)")
                     continue
                 } else {
+                    s += 1
                     pCarry = nil
+                    continue
                 }
-                if pChar == "." { continue }
-                else if sChar == pChar { continue }
-                else { return false }
+                if pChar == "." { s += 1; continue }
+                else if sChar == pChar { s += 1; continue }
+                else { print("=== C ==="); return false }
             }
         }
         return true
     }
 }
 
+
 let solution = Solution()
-let ans = solution.isMatch("ab", ".*c") // "mississippi" / "mis*is*ip*.", "aa" / "a*", "ab" / ".*c"
+let ans = solution.isMatch("aaa", "ab*ac*a") // "mississippi" / "mis*is*ip*.", "aa" / "a*", "ab" / ".*c", "aaa" / "ab*ac*a"
 print("ans: \(ans)")
