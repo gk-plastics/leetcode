0c0c901c5b980894aedbbc38f734e09f19b2c5e1
Go Kameda <go.kameda@icloud.com>
2019-06-01 16:36:44 +0900

    Fix implementation errors ( Now it covers the case of "ab" / ".*c" )

--- LeetCode.playground/Pages/RegularExpressionMatching.xcplaygroundpage/Contents.swift
+++ LeetCode.playground/Pages/RegularExpressionMatching.xcplaygroundpage/Contents.swift
@@ -4,13 +4,12 @@ final class Solution {
     func isMatch(_ s: String, _ p: String) -> Bool {
         let sChars = Array(s)
         let pChars = Array(p)
+
         var i = 0
-        var prevPChar: Character?
         var pCarry: Character?
         for sChar in sChars {
             print("sChar: \(sChar)")
             guard i < pChars.count else {
-                print("i is smaller than pChars.count")
                 if let pCarryChar = pCarry {
                     print("pCarryChar: \(pCarryChar)")
                     if pCarryChar == "." {
@@ -28,8 +27,15 @@ final class Solution {
             let pChar = pChars[i]
             print("pChar: \(pChar)")
             i += 1
+
             if pChar == "*" {
-                guard let pCarryChar = pCarry else { print("Invalid Operation: pCarry is nil."); return false }
+                guard let pCarryChar = pCarry else {
+                    //print("Invalid Operation: pCarry is nil.");
+                    return false
+                }
+                if i >= sChars.count {
+                    guard pChars[i] == "*" else { return false }
+                }
                 print("pCarryChar: \(pCarryChar)")
                 if pCarryChar == "." {
                     continue
@@ -38,7 +44,7 @@ final class Solution {
                 } else {
                     pCarry = nil
                     guard i < pChars.count else { return false }
-                    print("pChars[i]: \(pChars[i])")
+                    //print("pChars[i]: \(pChars[i])")
                     guard pChars[i] == sChar else { return false }
                 }
             } else {
@@ -58,5 +64,5 @@ final class Solution {
 }
 
 let solution = Solution()
-let ans = solution.isMatch("aa", "a") // "mississippi", "mis*is*ip*."
+let ans = solution.isMatch("ab", ".*c") // "mississippi", "mis*is*ip*."
 print("ans: \(ans)")
